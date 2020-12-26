ce85f3ac35924055d7bf444b2ac07f9e1604e061
chvjak <chvjak@gmailcom>
2020-12-26 18:36:49 +0100

    Upload something to blob

--- ImageHosting1/Function1.cs
+++ ImageHosting1/Function1.cs
@@ -7,29 +7,71 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 
-namespace ImageHosting1
+namespace AzFunctions
 {
-    public static class Function1
+    public static class UploadBlobHttpTriggerFunc
     {
-        [FunctionName("Function1")]
+        [FunctionName("UploadBlobHttpTriggerFunc")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+            ILogger log, ExecutionContext context)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation($"C# Http trigger function executed at: {DateTime.Now}");
+            CreateContainerIfNotExists(log, context);
 
-            string name = req.Query["name"];
+            CloudStorageAccount storageAccount = GetCloudStorageAccount(context);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference("dummy-messages");
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            string randomStr = Guid.NewGuid().ToString();
+            CloudBlockBlob blob = container.GetBlockBlobReference(randomStr);
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            var serializeJesonObject = JsonConvert.SerializeObject(new { ID = randomStr, Content = $"<html><body><h2> This is a Sample email content! </h2></body></html>" });
+            blob.Properties.ContentType = "application/json";
 
-            return new OkObjectResult(responseMessage);
+            using (var ms = new MemoryStream())
+            {
+                LoadStreamWithJson(ms, serializeJesonObject);
+                await blob.UploadFromStreamAsync(ms);
+            }
+
+            log.LogInformation($"Bolb {randomStr} is uploaded to container {container.Name}");
+            await blob.SetPropertiesAsync();
+
+            return new OkObjectResult("UploadBlobHttpTrigger function executed successfully!!");
+        }
+
+        private static void CreateContainerIfNotExists(ILogger logger, ExecutionContext executionContext)
+        {
+            CloudStorageAccount storageAccount = GetCloudStorageAccount(executionContext);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            string[] containers = new string[] { "dummy-messages" };
+            foreach (var item in containers)
+            {
+                CloudBlobContainer blobContainer = blobClient.GetContainerReference(item);
+                blobContainer.CreateIfNotExistsAsync();
+            }
+        }
+
+        private static CloudStorageAccount GetCloudStorageAccount(ExecutionContext executionContext)
+        {
+            var config = new ConfigurationBuilder()
+                            .SetBasePath(executionContext.FunctionAppDirectory)
+                            .AddJsonFile("local.settings.json", true, true)
+                            .AddEnvironmentVariables().Build();
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(config["CloudStorageAccount"]);
+            return storageAccount;
+        }
+        private static void LoadStreamWithJson(Stream ms, object obj)
+        {
+            StreamWriter writer = new StreamWriter(ms);
+            writer.Write(obj);
+            writer.Flush();
+            ms.Position = 0;
         }
     }
 }
